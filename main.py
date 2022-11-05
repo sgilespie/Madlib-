a8b10e7005b693987cb557dd9d159c611435126d
Samantha Gillespie <110204021+sgilespie@users.noreply.github.com>
2022-11-05 15:02:39 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+a = input("Enter an adjective:")
+first = ",cold November day."
+print("It was a " + a + first)
+b = input("Enter second adjective:")
+second = " smell of "
+c = input("Enter a type of bird:")
+print("I woke up to the " + b + second + c)
+d = input("Enter room in the house:")
+third = "roasting in the "
+print(third + d + " downstairs.")
+e = input("Enter a past tense verb:")
+print("I " + e + " down the stairs " + "to see if I could help dinner.")
+f = input("Enter a relative's name:")
+g = input("Enter a noun:")
+print("My mom said, 'See if " + f + " needs a fresh " + g + ".'")
+h = input("Enter a liquid:")
+i = input("Enter a verb ending in -ing:")
+print("So I carried a tray of glasses full of " + h + " into the " + i + " room.")
+j = input("Enter a body part(plural):")
+print("When I got there, I couldn't believe my " + j + "!")
+k = input("Enter a plural noun:")
+n = input("Enter a verb ending in -ing:")
+m = input("Enter a noun:")
+print("There were " + k + " " + n + " on the " + m + "!")
+
+
+
+
+
+
+
+
+
+
+
